1b8500f8e484223bfdbcca6eb8fbec0db3489a16
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2021-04-28 00:10:52 +0300

    Homework updated

--- secondDayHomework/src/secondDayHomework/LecturerManager.java
+++ secondDayHomework/src/secondDayHomework/LecturerManager.java
@@ -2,9 +2,9 @@ package secondDayHomework;
 
 public class LecturerManager {
 	
-	public void LecturerInfo(Lecturer lecturer) {
-		System.out.println("Hello, I'm" + lecturer.lecturerName + 
-				"have MCT, PMP and ITIL certification" + lecturer.lecturerTitle);
+	public void lecturerInfo(Lecturer lecturer) {
+		System.out.println("Hello, I'm " + lecturer.lecturerName + 
+				" have MCT, PMP and ITIL certification " + lecturer.lecturerTitle);
 	} 
 }
 
--- secondDayHomework/src/secondDayHomework/Main.java
+++ secondDayHomework/src/secondDayHomework/Main.java
@@ -25,7 +25,33 @@ public class Main {
 			System.out.println(lecturer.lecturerName + " - " + lecturer.lecturerTitle);
 		}
 		
+		Category category1 = new Category(101, "Java - React.js Bootcamp",
+				"Java and React Fullstack Software Developer Trainer Camp", 0);
 		
+		Category category2 = new Category(102, ".Net - Angular.js Bootcamp",
+				".Net and Angular Fullstack Software Developer Trainer Camp", 0);
+		
+		System.out.println(" ");
+		
+		Category[] categories = {category1, category2};
+		
+		System.out.println("Categories");
+		System.out.println("===============================");
+		
+		for(Category category : categories) {
+			System.out.println(category.categoryDescription);
+		}
+		
+		System.out.println(" ");
+		System.out.println(" ");
+		
+		LecturerManager lecturerManager1 = new LecturerManager();
+		lecturerManager1.lecturerInfo(lecturer1);
+		
+		System.out.println(" ");
+		
+		LecturerManager lecturerManager2 = new LecturerManager();
+		lecturerManager2.lecturerInfo(lecturer2);
 	}
 
 }
